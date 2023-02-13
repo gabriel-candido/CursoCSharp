60a8cfc88d4590acba4af3adb08947495124b656
Gabs Candido <73241602+gabriel-candido@users.noreply.github.com>
2023-02-13 09:28:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ola mundo");
+        Console.WriteLine("Tecle qualquer tecla pra sair...");
+        Console.ReadLine();
+    }
+}
